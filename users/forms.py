291927d82315dc88308457c3d989e276187586c0
A <alyssa@kvasl.com>
2023-08-16 11:34:38 +1200

    Added temp templates

---
+++ users/forms.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = CustomUser
+        # fields = UserCreationForm.Meta.fields + ("age", )
+        fields = (
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email", 
+            "position",
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        # fields = UserChangeForm.Meta.fields
+        fields = (
+            "username",
+            "email", 
+            "age",
+        )
