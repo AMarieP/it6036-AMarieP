b795c7b9b7ddee966be7e8b0e14042f517ae07bb
A <alyssa@kvasl.com>
2023-08-16 16:04:33 +1200

    created a base home page, set so it is displayed

--- gonz/urls.py
+++ gonz/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")), #access login, sign up etc
-    path('', include('users.urls'))#access sign up sheet
+    path('', include('users.urls')), #access sign up sheet
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # Allows Home to be views as home template
 ]
